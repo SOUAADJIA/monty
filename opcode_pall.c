1190da2bd5d4e527916dfa7f1a69c9ac2340b146
Souaad OULEDJIA <souadouledjia@gmail.com>
2023-09-21 12:04:25 -0700

    pall fixed

--- opcode_pall.c
+++ opcode_pall.c
@@ -3,30 +3,21 @@
 #include <stdlib.h>
 
 /**
- * op_pall - Removes the top element of the stack.
+ * op_pall - Prints all the values on the stack, starting from the top.
  * @stack: A pointer to the top of the stack.
  * @line_number: The current line number in the Monty bytecode file.
- * Return: nothing
+ * Return: Nothing.
  */
 void op_pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp;
+	stack_t *temp = *stack;
 
-	if (*stack == NULL)
+	(void)line_number; /* Unused parameter */
+
+	while (temp != NULL)
 	{
-		fprintf(stderr, "L%d: can't pull an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	if ((*stack)->next != NULL)
-	{
-		temp = (*stack)->next;
-		temp->prev = NULL;
-		free(*stack);
-		*stack = temp;
-	}
-	else
-	{
-		free(*stack);
-		*stack = NULL;
+		printf("%d\n", temp->n);
+		temp = temp->next;
 	}
 }
+
